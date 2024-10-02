c0638401e8f0e66d3ec6a829c803fda15f8f7343
Madhu Kandukuri <70106840+Madhu683@users.noreply.github.com>
2024-10-02 12:36:18 +0000

    adding fetch operation

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 
-let todos = []
+let todos = [
+    {
+    id:1,
+    title: 'Create a method',
+    description: ' Create a method to perform add operation in that function',
+    completed: 'false'
+    },
+    {id:2,
+        title: 'Create a method',
+        description: ' Create a method to perform add operation in that function',
+        completed: 'false'
+    }
+]
 
 // creating a new todo
 app.post('/todos',(req,res)=>{
@@ -22,8 +34,17 @@ app.post('/todos',(req,res)=>{
 })
 
 app.get('/todos',(req,res)=>{
-    res.send('Todos')
-    console.log('Todos')
+    //res.send('Todos')
+    //console.log('Todos')
+
+    let todosString = ''
+    for(let i = 0;i<todos.length;i++)
+        todosString+=`<h3>ID:${todos[i].id} Title: ${todos[i].title} Description: ${todos[i].description}</h3>`
+  
+    console.log(todosString)
+    res.send(todosString)
+    
+  // res.json(todos)
 })
 app.listen(5000,()=>{
     console.log('Server is listening on port 5000...')
